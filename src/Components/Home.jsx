b9455fe5322638913144a0f5bfffd721eae7c7cb
toqa-saad97 <toqasaad1997@gmail.com>
2024-11-13 18:49:22 +0200

    feat: :zap: add the Trending & originals  and new component

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,6 +1,10 @@
 import styled from "styled-components";
 import ImgSlider from './ImgSlider';
 import Viewers from "./Viewers";
+import Recommends from "./Recommends";
+import NewDisney from "./NewDisney";
+import Originals from "./Originals";
+import Trending from "./Trending";
 
 
 
@@ -12,6 +16,10 @@ const Home = () => {
     <Container>
       <ImgSlider/>
       <Viewers/>
+      <Recommends />
+      <NewDisney />
+      <Originals />
+      <Trending />
 
 
     </Container>
