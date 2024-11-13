c28bdb71eb1cf4ee9ba862804bc41b7f89f60170
toqa-saad97 <toqasaad1997@gmail.com>
2024-11-13 19:29:40 +0200

    feat: :zap: Refactor movie categorization using reduce in useEffect

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,27 +1,59 @@
 import styled from "styled-components";
-import ImgSlider from './ImgSlider';
+import ImgSlider from "./ImgSlider";
 import Viewers from "./Viewers";
 import Recommends from "./Recommends";
 import NewDisney from "./NewDisney";
 import Originals from "./Originals";
 import Trending from "./Trending";
-
-
-
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import db from "../firebase";
+import { setMovies } from "../features/movie/movieSlice";
+import { selectUserName } from "../features/user/userSlice";
 
 const Home = () => {
+  const dispatch = useDispatch();
+  const userName = useSelector(selectUserName);
 
+  useEffect(() => {
+    db.collection("movies").onSnapshot((snapshot) => {
+      const movies = snapshot.docs.reduce(
+        (acc, doc) => {
+          const data = { id: doc.id, ...doc.data() };
+          switch (data.type) {
+            case "recommend":
+              acc.recommend.push(data);
+              break;
+            case "new":
+              acc.newDisney.push(data);
+              break;
+            case "original":
+              acc.original.push(data);
+              break;
+            case "trending":
+              acc.trending.push(data);
+              break;
+            default:
+              break;
+          }
+          return acc;
+        },
+        { recommend: [], newDisney: [], original: [], trending: [] }
+      );
+
+
+      dispatch(setMovies(movies));
+    });
+  }, [userName, dispatch]);
 
   return (
     <Container>
-      <ImgSlider/>
-      <Viewers/>
+      <ImgSlider />
+      <Viewers />
       <Recommends />
       <NewDisney />
       <Originals />
       <Trending />
-
-
     </Container>
   );
 };
@@ -32,7 +64,7 @@ const Container = styled.main`
   overflow-x: hidden;
   display: block;
   top: 72px;
-  margin: 0px 24px;
+  padding: 0 calc(3.5vw + 5px);
 
   &:after {
     background: url("/images/home-background.png") center center / cover
